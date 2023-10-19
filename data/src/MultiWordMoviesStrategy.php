92951aa9800d95839a4bd5acea898facab1d2640
Michał Sioła <michal152.1996@gmail.com>
2023-10-19 11:19:55 +0200

    Change array() to []

--- data/src/MultiWordMoviesStrategy.php
+++ data/src/MultiWordMoviesStrategy.php
@@ -6,7 +6,7 @@ class MultiWordMoviesStrategy implements RecommendationStrategyInterface
 {
     public function recommend(array $movies): array
     {
-        $result = array();
+        $result = [];
         foreach ($movies as $movie) {
             if (str_contains($movie, ' ')) {
                 $result[] = $movie;
